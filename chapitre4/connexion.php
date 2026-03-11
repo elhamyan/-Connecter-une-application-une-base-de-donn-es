3faa6de6506bf64f7cd7606c5da3a73ca94fc2ce
aziza <azizaelhamyani40@gmail.com>
2026-03-11 12:19:33 +0000

    chapitre4

--- chapitre4/connexion.php
+++ chapitre4/connexion.php
@@ -7,21 +7,15 @@ $password='';
 try{
     $pdo= new PDO('mysql:host=localhost;dbname=blogdb','root','');
     $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    echo "connexion réussée";
+    echo "connexion réussie";
 }catch(PDOException $e){
-    echo "Errure : ". $e->getMessage();
+    echo "Erreur : ". $e->getMessage();
 }
 
 try {
-    $pdo->query("SELECT * FROM Utilisateurfff");
+    $pdo->query("SELECT * FROM table_inexistante");
 } catch (PDOException $e) {
-    echo "Erreur SQL : " . $e->getMessage();
-}
-
-catch (PDOException $e) {
-    file_put_contents('erreurs.log', $e->getMessage(), FILE_APPEND);
+    file_put_contents('erreurs.log', $e->getMessage().PHP_EOL, FILE_APPEND);
     echo "Une erreur est survenue. Contactez l'administrateur.";
 }
 
-
-?>
